356ee95d9b1582ced73f5b2b741398a56cd34c93
job-lukasz <job.lukasz.1990@gmail.com>
2014-06-02 13:23:13 +0200

    initial commit archetype adaptation

---
+++ src/main/java/spring/mvc/account/AccountRepository.java
@@ -0,0 +1,43 @@
+package spring.mvc.account;
+
+import javax.inject.Inject;
+import javax.persistence.PersistenceException;
+
+import org.hibernate.SessionFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Propagation;
+import org.springframework.transaction.annotation.Transactional;
+
+
+@Repository
+@Transactional(readOnly = false, propagation = Propagation.REQUIRES_NEW)
+public class AccountRepository {
+
+	// @PersistenceContext
+	// private EntityManager entityManager;
+	@Autowired
+	private SessionFactory sessionFactory;
+
+	@Inject
+	private PasswordEncoder passwordEncoder;
+
+	@Transactional
+	public Account save(Account account) {
+		account.setPassword(passwordEncoder.encode(account.getPassword()));
+		sessionFactory.getCurrentSession().saveOrUpdate(account);
+		return account;
+	}
+
+	public Account findByEmail(String email) {
+		try {
+			Account acc = (Account) sessionFactory.getCurrentSession().createQuery("From Account Where email = :email")
+					.setParameter("email", email).uniqueResult();
+			return acc;
+		} catch (PersistenceException e) {
+			return null;
+		}
+	}
+
+}
---
+++ src/main/java/spring/mvc/home/HomeController.java
@@ -0,0 +1,29 @@
+package spring.mvc.home;
+
+import java.security.Principal;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import spring.mvc.account.AccountRepository;
+import spring.mvc.signup.SignupForm;
+@Controller
+public class HomeController {
+	@Autowired
+	private AccountRepository accountRepository;
+	
+	//ten kontroler powinnien być jak najmniejszy!
+	@RequestMapping(value = "/", method = RequestMethod.GET)
+	public String index(Principal principal, Model model) {
+		new SignupForm();
+		if(principal != null){
+			model.addAttribute("name",principal.getName());
+			return "home/homeSignedIn";
+		}
+
+		return "home/homeNotSignedIn";
+	}
+}
