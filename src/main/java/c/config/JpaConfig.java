df61118c13e47a811dac694c7d582347386a49c2
job-lukasz <job.lukasz.1990@gmail.com>
2015-04-08 20:02:05 +0200

    fix code after refactor

--- src/main/java/c/config/JpaConfig.java
+++ src/main/java/c/config/JpaConfig.java
@@ -47,7 +47,7 @@ public class JpaConfig {
 
 		LocalSessionFactoryBean sessionFactory = new LocalSessionFactoryBean();
 		sessionFactory.setDataSource(dataSource);
-		sessionFactory.setPackagesToScan(new String[] {"spring.mvc.account", "spring.mvc.measurement"});
+		sessionFactory.setPackagesToScan(new String[] {"c.account", "c.measurement"});
 		sessionFactory.setHibernateProperties(hibernateProperties().getObject());
 
 		return sessionFactory;
