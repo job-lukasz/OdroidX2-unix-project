f5617ec5c2ab5f2d7813993cf2dbdc370bb18a4c
job-lukasz <job.lukasz.1990@gmail.com>
2015-04-08 20:33:04 +0200

    uncomment all tests

--- src/test/java/c/account/UserServiceTest.java
+++ src/test/java/c/account/UserServiceTest.java
@@ -1,70 +1,70 @@
-//package c.account;
-//
-//import static org.fest.assertions.Assertions.assertThat;
-//import static org.mockito.Mockito.when;
-//
-//import java.util.Collection;
-//
-//import org.junit.Rule;
-//import org.junit.Test;
-//import org.junit.rules.ExpectedException;
-//import org.junit.runner.RunWith;
-//import org.mockito.InjectMocks;
-//import org.mockito.Mock;
-//import org.mockito.runners.MockitoJUnitRunner;
-//import org.springframework.security.core.GrantedAuthority;
-//import org.springframework.security.core.userdetails.UserDetails;
-//import org.springframework.security.core.userdetails.UsernameNotFoundException;
-//
-//import c.account.Account;
-//import c.account.AccountRepository;
-//import c.account.UserService;
-//
-//@RunWith(MockitoJUnitRunner.class)
-//public class UserServiceTest {
-//
-//	@InjectMocks
-//	private UserService userService = new UserService();
-//
-//	@Mock
-//	private AccountRepository accountRepositoryMock;
-//
-//	@Rule
-//	public ExpectedException thrown = ExpectedException.none();
-//
-//	@Test
-//	public void shouldThrowExceptionWhenUserNotFound() {
-//		// arrange
-//		thrown.expect(UsernameNotFoundException.class);
-//		thrown.expectMessage("user not found");
-//
-//		when(accountRepositoryMock.findByEmail("user@example.com")).thenReturn(null);
-//		// act
-//		userService.loadUserByUsername("user@example.com");
-//	}
-//
-//	@Test
-//	public void shouldReturnUserDetails() {
-//		// arrange
-//		Account demoUser = new Account("user@example.com", "demo", "ROLE_USER");
-//		when(accountRepositoryMock.findByEmail("user@example.com")).thenReturn(demoUser);
-//
-//		// act
-//		UserDetails userDetails = userService.loadUserByUsername("user@example.com");
-//
-//		// assert
-//		assertThat(demoUser.getEmail()).isEqualTo(userDetails.getUsername());
-//		assertThat(demoUser.getPassword()).isEqualTo(userDetails.getPassword());
-//        assertThat(hasAuthority(userDetails, demoUser.getRole()));
-//	}
-//
-//	private boolean hasAuthority(UserDetails userDetails, String role) {
-//		Collection<? extends GrantedAuthority> authorities = userDetails.getAuthorities();
-//		for(GrantedAuthority authority : authorities) {
-//			if(authority.getAuthority().equals(role)) {
-//				return true;
-//			}
-//		}
-//		return false;
-//	}
-//}
+package c.account;
+
+import static org.fest.assertions.Assertions.assertThat;
+import static org.mockito.Mockito.when;
+
+import java.util.Collection;
+
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.rules.ExpectedException;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.runners.MockitoJUnitRunner;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+
+import c.account.Account;
+import c.account.AccountRepository;
+import c.account.UserService;
+
+@RunWith(MockitoJUnitRunner.class)
+public class UserServiceTest {
+
+	@InjectMocks
+	private UserService userService = new UserService();
+
+	@Mock
+	private AccountRepository accountRepositoryMock;
+
+	@Rule
+	public ExpectedException thrown = ExpectedException.none();
+
+	@Test
+	public void shouldThrowExceptionWhenUserNotFound() {
+		// arrange
+		thrown.expect(UsernameNotFoundException.class);
+		thrown.expectMessage("user not found");
+
+		when(accountRepositoryMock.findByEmail("user@example.com")).thenReturn(null);
+		// act
+		userService.loadUserByUsername("user@example.com");
+	}
+
+	@Test
+	public void shouldReturnUserDetails() {
+		// arrange
+		Account demoUser = new Account("user@example.com", "demo", "ROLE_USER");
+		when(accountRepositoryMock.findByEmail("user@example.com")).thenReturn(demoUser);
+
+		// act
+		UserDetails userDetails = userService.loadUserByUsername("user@example.com");
+
+		// assert
+		assertThat(demoUser.getEmail()).isEqualTo(userDetails.getUsername());
+		assertThat(demoUser.getPassword()).isEqualTo(userDetails.getPassword());
+        assertThat(hasAuthority(userDetails, demoUser.getRole()));
+	}
+
+	private boolean hasAuthority(UserDetails userDetails, String role) {
+		Collection<? extends GrantedAuthority> authorities = userDetails.getAuthorities();
+		for(GrantedAuthority authority : authorities) {
+			if(authority.getAuthority().equals(role)) {
+				return true;
+			}
+		}
+		return false;
+	}
+}
