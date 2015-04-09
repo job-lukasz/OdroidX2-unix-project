50df5637b5a9024b0755709c8199244a95569907
job-lukasz <job.lukasz.1990@gmail.com>
2015-04-09 21:38:26 +0200

    add all settings to model and config

--- src/main/java/c/settings/SettingsForm.java
+++ src/main/java/c/settings/SettingsForm.java
@@ -1,8 +1,171 @@
 package c.settings;
 
+import m.gpio.StaticValues.OdroidX2PIN;
+import m.settings.Settings;
+
 public class SettingsForm {
 
 	private String temperaturePortName;
+	private OdroidX2PIN firstHeaterPin;
+	private OdroidX2PIN secondHeaterPin;
+	
+	private OdroidX2PIN mainValvePin;
+	private OdroidX2PIN cleanWaterValvePin;
+	private OdroidX2PIN runningWaterValvePin;
+	private OdroidX2PIN wortValvePin;
+	
+	private OdroidX2PIN wortPompPin;
+	
+	private OdroidX2PIN firstMainCapacityLevel;
+	private OdroidX2PIN secondMainCapacityLevel;
+	private OdroidX2PIN thirdMainCapacityLevel;
+	private OdroidX2PIN fourthMainCapacityLevel;
+	private OdroidX2PIN firstCleanCapacityLevel;
+	private OdroidX2PIN secondCleanCapacityLevel;
+	private OdroidX2PIN thirdCleanCapacityLevel;
+	private OdroidX2PIN fourthCleanCapacityLevel;
+	
+
+	public SettingsForm(){
+		temperaturePortName = Settings.getTemperaturePortName();
+		firstHeaterPin = Settings.getFirstHeaterPin();
+		secondHeaterPin = Settings.getSecondHeaterPin();
+		
+		mainValvePin = Settings.getMainValvePin();
+		cleanWaterValvePin = Settings.getCleanWaterValvePin();
+		runningWaterValvePin = Settings.getRunningWaterValvePin();
+		wortValvePin = Settings.getWortValvePin();
+		
+		wortPompPin = Settings.getWortPompPin();
+		
+		firstMainCapacityLevel = Settings.getFirstMainCapacityLevel();
+		secondMainCapacityLevel = Settings.getSecondMainCapacityLevel();
+		thirdMainCapacityLevel = Settings.getThirdMainCapacityLevel();
+		fourthMainCapacityLevel = Settings.getFourthMainCapacityLevel();
+		firstCleanCapacityLevel = Settings.getFirstCleanCapacityLevel();
+		secondCleanCapacityLevel = Settings.getSecondCleanCapacityLevel();
+		thirdCleanCapacityLevel = Settings.getThirdCleanCapacityLevel();
+		fourthCleanCapacityLevel = Settings.getFirstCleanCapacityLevel();
+	}
+	
+	public OdroidX2PIN getRunningWaterValvePin() {
+		return runningWaterValvePin;
+	}
+
+	public void setRunningWaterValvePin(OdroidX2PIN runningWaterValvePin) {
+		this.runningWaterValvePin = runningWaterValvePin;
+	}
+
+	public OdroidX2PIN getWortPompPin() {
+		return wortPompPin;
+	}
+
+	public void setWortPompPin(OdroidX2PIN wortPompPin) {
+		this.wortPompPin = wortPompPin;
+	}
+
+	public OdroidX2PIN getFirstMainCapacityLevel() {
+		return firstMainCapacityLevel;
+	}
+
+	public void setFirstMainCapacityLevel(OdroidX2PIN firstMainCapacityLevel) {
+		this.firstMainCapacityLevel = firstMainCapacityLevel;
+	}
+
+	public OdroidX2PIN getSecondMainCapacityLevel() {
+		return secondMainCapacityLevel;
+	}
+
+	public void setSecondMainCapacityLevel(OdroidX2PIN secondMainCapacityLevel) {
+		this.secondMainCapacityLevel = secondMainCapacityLevel;
+	}
+
+	public OdroidX2PIN getThirdMainCapacityLevel() {
+		return thirdMainCapacityLevel;
+	}
+
+	public void setThirdMainCapacityLevel(OdroidX2PIN thirdMainCapacityLevel) {
+		this.thirdMainCapacityLevel = thirdMainCapacityLevel;
+	}
+
+	public OdroidX2PIN getFourthMainCapacityLevel() {
+		return fourthMainCapacityLevel;
+	}
+
+	public void setFourthMainCapacityLevel(OdroidX2PIN fourthMainCapacityLevel) {
+		this.fourthMainCapacityLevel = fourthMainCapacityLevel;
+	}
+
+	public OdroidX2PIN getFirstCleanCapacityLevel() {
+		return firstCleanCapacityLevel;
+	}
+
+	public void setFirstCleanCapacityLevel(OdroidX2PIN firstCleanCapacityLevel) {
+		this.firstCleanCapacityLevel = firstCleanCapacityLevel;
+	}
+
+	public OdroidX2PIN getSecondCleanCapacityLevel() {
+		return secondCleanCapacityLevel;
+	}
+
+	public void setSecondCleanCapacityLevel(OdroidX2PIN secondCleanCapacityLevel) {
+		this.secondCleanCapacityLevel = secondCleanCapacityLevel;
+	}
+
+	public OdroidX2PIN getThirdCleanCapacityLevel() {
+		return thirdCleanCapacityLevel;
+	}
+
+	public void setThirdCleanCapacityLevel(OdroidX2PIN thirdCleanCapacityLevel) {
+		this.thirdCleanCapacityLevel = thirdCleanCapacityLevel;
+	}
+
+	public OdroidX2PIN getFourthCleanCapacityLevel() {
+		return fourthCleanCapacityLevel;
+	}
+
+	public void setFourthCleanCapacityLevel(OdroidX2PIN fourthCleanCapacityLevel) {
+		this.fourthCleanCapacityLevel = fourthCleanCapacityLevel;
+	}
+
+	public OdroidX2PIN getFirstHeaterPin() {
+		return firstHeaterPin;
+	}
+	public void setFirstHeaterPin(OdroidX2PIN firstHeaterPin) {
+		this.firstHeaterPin = firstHeaterPin;
+	}
+	
+	public OdroidX2PIN getSecondHeaterPin() {
+		return secondHeaterPin;
+	}
+	
+	public void setSecondHeaterPin(OdroidX2PIN secondHeaterPin) {
+		this.secondHeaterPin = secondHeaterPin;
+	}
+	
+	public OdroidX2PIN getMainValvePin() {
+		return mainValvePin;
+	}
+	
+	public void setMainValvePin(OdroidX2PIN mainValvePin) {
+		this.mainValvePin = mainValvePin;
+	}
+	
+	public OdroidX2PIN getCleanWaterValvePin() {
+		return cleanWaterValvePin;
+	}
+	
+	public void setCleanWaterValvePin(OdroidX2PIN cleanWaterValvePin) {
+		this.cleanWaterValvePin = cleanWaterValvePin;
+	}
+	
+	public OdroidX2PIN getWortValvePin() {
+		return wortValvePin;
+	}
+	
+	public void setWortValvePin(OdroidX2PIN wortValvePin) {
+		this.wortValvePin = wortValvePin;
+	}
 	
 	public String getTemperaturePortName() {
 		return temperaturePortName;
--- src/main/java/m/settings/Settings.java
+++ src/main/java/m/settings/Settings.java
@@ -1,12 +1,155 @@
 package m.settings;
 
+import m.gpio.StaticValues.OdroidX2PIN;
+
 public class Settings {
-	private static String temperaturePortSensor;
-	public static String getTemperaturePortSensor() {
-		return temperaturePortSensor;
+	
+	private static String temperaturePortName;
+	private static OdroidX2PIN firstHeaterPin;
+	private static OdroidX2PIN secondHeaterPin;
+	
+	private static OdroidX2PIN mainValvePin;
+	private static OdroidX2PIN cleanWaterValvePin;
+	private static OdroidX2PIN runningWaterValvePin;
+	private static OdroidX2PIN wortValvePin;
+	
+	private static OdroidX2PIN wortPompPin;
+	
+	private static OdroidX2PIN firstMainCapacityLevel;
+	private static OdroidX2PIN secondMainCapacityLevel;
+	private static OdroidX2PIN thirdMainCapacityLevel;
+	private static OdroidX2PIN fourthMainCapacityLevel;
+
+	private static OdroidX2PIN firstCleanCapacityLevel;
+	private static OdroidX2PIN secondCleanCapacityLevel;
+	private static OdroidX2PIN thirdCleanCapacityLevel;
+	private static OdroidX2PIN fourthCleanCapacityLevel;
+	
+	public static String getTemperaturePortName() {
+		return temperaturePortName;
 	}
 
-	public static void setTemperaturePortSensor(String temperaturePortSensor) {
-		Settings.temperaturePortSensor = temperaturePortSensor;
+	public static void setTemperaturePortName(String temperaturePortName) {
+		Settings.temperaturePortName = temperaturePortName;
+	}
+
+	public static OdroidX2PIN getFirstHeaterPin() {
+		return firstHeaterPin;
+	}
+
+	public static void setFirstHeaterPin(OdroidX2PIN firstHeaterPin) {
+		Settings.firstHeaterPin = firstHeaterPin;
+	}
+
+	public static OdroidX2PIN getSecondHeaterPin() {
+		return secondHeaterPin;
+	}
+
+	public static void setSecondHeaterPin(OdroidX2PIN secondHeaterPin) {
+		Settings.secondHeaterPin = secondHeaterPin;
+	}
+
+	public static OdroidX2PIN getMainValvePin() {
+		return mainValvePin;
+	}
+
+	public static void setMainValvePin(OdroidX2PIN mainValvePin) {
+		Settings.mainValvePin = mainValvePin;
+	}
+
+	public static OdroidX2PIN getCleanWaterValvePin() {
+		return cleanWaterValvePin;
+	}
+
+	public static void setCleanWaterValvePin(OdroidX2PIN cleanWaterValvePin) {
+		Settings.cleanWaterValvePin = cleanWaterValvePin;
+	}
+
+	public static OdroidX2PIN getRunningWaterValvePin() {
+		return runningWaterValvePin;
+	}
+
+	public static void setRunningWaterValvePin(OdroidX2PIN runningWaterValvePin) {
+		Settings.runningWaterValvePin = runningWaterValvePin;
+	}
+
+	public static OdroidX2PIN getWortValvePin() {
+		return wortValvePin;
+	}
+
+	public static void setWortValvePin(OdroidX2PIN wortValvePin) {
+		Settings.wortValvePin = wortValvePin;
+	}
+
+	public static OdroidX2PIN getWortPompPin() {
+		return wortPompPin;
+	}
+
+	public static void setWortPompPin(OdroidX2PIN wortPompPin) {
+		Settings.wortPompPin = wortPompPin;
+	}
+
+	public static OdroidX2PIN getFirstMainCapacityLevel() {
+		return firstMainCapacityLevel;
+	}
+
+	public static void setFirstMainCapacityLevel(OdroidX2PIN firstMainCapacityLevel) {
+		Settings.firstMainCapacityLevel = firstMainCapacityLevel;
+	}
+
+	public static OdroidX2PIN getSecondMainCapacityLevel() {
+		return secondMainCapacityLevel;
+	}
+
+	public static void setSecondMainCapacityLevel(OdroidX2PIN secondMainCapacityLevel) {
+		Settings.secondMainCapacityLevel = secondMainCapacityLevel;
+	}
+
+	public static OdroidX2PIN getThirdMainCapacityLevel() {
+		return thirdMainCapacityLevel;
+	}
+
+	public static void setThirdMainCapacityLevel(OdroidX2PIN thirdMainCapacityLevel) {
+		Settings.thirdMainCapacityLevel = thirdMainCapacityLevel;
+	}
+
+	public static OdroidX2PIN getFourthMainCapacityLevel() {
+		return fourthMainCapacityLevel;
+	}
+
+	public static void setFourthMainCapacityLevel(OdroidX2PIN fourthMainCapacityLevel) {
+		Settings.fourthMainCapacityLevel = fourthMainCapacityLevel;
+	}
+
+	public static OdroidX2PIN getFirstCleanCapacityLevel() {
+		return firstCleanCapacityLevel;
+	}
+
+	public static void setFirstCleanCapacityLevel(OdroidX2PIN firstCleanCapacityLevel) {
+		Settings.firstCleanCapacityLevel = firstCleanCapacityLevel;
+	}
+
+	public static OdroidX2PIN getSecondCleanCapacityLevel() {
+		return secondCleanCapacityLevel;
+	}
+
+	public static void setSecondCleanCapacityLevel(OdroidX2PIN secondCleanCapacityLevel) {
+		Settings.secondCleanCapacityLevel = secondCleanCapacityLevel;
+	}
+
+	public static OdroidX2PIN getThirdCleanCapacityLevel() {
+		return thirdCleanCapacityLevel;
+	}
+
+	public static void setThirdCleanCapacityLevel(OdroidX2PIN thirdCleanCapacityLevel) {
+		Settings.thirdCleanCapacityLevel = thirdCleanCapacityLevel;
+	}
+
+	public static OdroidX2PIN getFourthCleanCapacityLevel() {
+		return fourthCleanCapacityLevel;
+	}
+
+	public static void setFourthCleanCapacityLevel(OdroidX2PIN fourthCleanCapacityLevel) {
+		Settings.fourthCleanCapacityLevel = fourthCleanCapacityLevel;
 	}
 }
